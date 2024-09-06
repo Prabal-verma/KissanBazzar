2813a201736fe601d447f2bbeb0ece6fe65aeda6
Prabal Verma <prableverma@gmail.com>
2024-09-07 01:13:40 +0530

    teailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,20 @@
-/** @type {import('tailwindcss').Config} */
+// tailwind.config.js
 module.exports = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        primary: '#1D4ED8',
+        secondary: '#3B82F6',
+        background: '#F9FAFB',
+        text: '#111827',
+      },
+      fontFamily: {
+        sans: ['Roboto', 'Arial', 'sans-serif'],
       },
     },
   },
   plugins: [],
-};
+}
